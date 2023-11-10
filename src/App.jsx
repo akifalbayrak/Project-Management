b6065f5845f609520bcd1638e761dd92a030b066
Akif Albayrak <akifalbayrak6161@gmail.com>
2023-11-10 10:17:16 +0300

    finishing app

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,94 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
         projects: [],
+        tasks:[]
     });
+
+    function handleAddTask(text){
+      setProjectsState((prevState) => {
+        const taskId = Math.random();
+        const newTask = {
+            text:text,
+            projectId:prevState.selectedProjectId,
+            id: taskId,
+        };
+        return {
+            ...prevState,
+            tasks:[newTask,...prevState.tasks]
+        };
+    });
+    }
+    function handleDeleteTask(id){
+      setProjectsState((prevState) => {
+        return {
+            ...prevState,
+            tasks:prevState.tasks.filter((task)=>task.id!==id)
+        };
+    });
+    }
+
     function handleStartAddProject() {
         setProjectsState((prevState) => {
             return {
-              ...prevState,
-              selectedProjectId: null,
+                ...prevState,
+                selectedProjectId: null,
             };
         });
     }
-    function handleAddProject(projectData){
-      setProjectsState(prevState=> {
-        const projectId = Math.random();
-        const newProject = {
-          ...projectData,
-          id:projectId
-        }
+
+    function handleDeleteProject(){
+      setProjectsState((prevState) => {
         return {
-          ...prevState,
-          selectedProjectId:undefined,
-          projects:[...prevState.projects,newProject]
-        }
-      })
+            ...prevState,
+            selectedProjectId: undefined,
+            projects:prevState.projects.filter((project)=>project.id!==prevState.selectedProjectId)
+        };
+    });
     }
-    let content;
+
+    function handleSelectProject(id){
+      setProjectsState((prevState) => {
+        return {
+            ...prevState,
+            selectedProjectId: id,
+        };
+    });
+    }
+
+    function handleCancelAddProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+            };
+        });
+    }
+
+    function handleAddProject(projectData) {
+        setProjectsState((prevState) => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId,
+            };
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: [...prevState.projects, newProject],
+            };
+        });
+    }
+    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} tasks={projectsState.tasks} />;
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject onAdd={handleAddProject} />;
+        content = <NewProject onCancel={handleCancelAddProject} onAdd={handleAddProject} />;
     } else if (projectsState.selectedProjectId === undefined) {
         content = (
             <NoProjectSelected onStartAddProject={handleStartAddProject} />
@@ -41,7 +98,12 @@ function App() {
 
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+            <ProjectsSidebar
+                selectedProjectId={projectsState.selectedProjectId}
+                onSelectProject = {handleSelectProject}
+                onStartAddProject={handleStartAddProject}
+                projects={projectsState.projects}
+            />
             {content}
         </main>
     );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,30 +1,60 @@
-import { useRef } from "react"
-import Input from "./Input"
-export default function NewProject({onAdd}){
+import { useRef } from "react";
+import Input from "./Input";
+import Modal from "./Modal"
+export default function NewProject({ onAdd,onCancel }) {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
 
-    function handleSave(){
+    function handleSave() {
         const enteredTitle = title.current.value;
         const enteredDesc = description.current.value;
         const enteredDate = dueDate.current.value;
+
+        if (
+            enteredTitle.trim() === "" ||
+            enteredDesc.trim() === "" ||
+            enteredDate.trim() === ""
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
-            title:enteredTitle,
-            description:enteredDesc,
-            dueDate:enteredDate
-        })
+            title: enteredTitle,
+            description: enteredDesc,
+            dueDate: enteredDate,
+        });
     }
 
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-5 my-4">
-            <li><button className="text-stone-800 hover:text-stone-900">Cancel</button></li>
-            <li><button onClick={handleSave} className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
-        </menu>
-        <div>
-            <Input type="text" ref={title} label="Title" />
-            <Input ref={description} label="Description" textarea />
-            <Input type="date" ref={dueDate} label="Due Date" />
-        </div>
-    </div>
+    return (
+        <>
+        <Modal ref={modal} buttonCaption="Okey">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-xl font-bold text-stone-600 my-4">Please provide a value for every input value</p>
+        </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-5 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-900" onClick={onCancel}>
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
+            </div>
+        </>
+    );
 }
